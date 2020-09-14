4ccfd0d277d2d33550ba573240e9ba51d3c84c47
bayupermadi <irwanhikmah@gmail.com>
2020-09-14 10:54:36 +0700

    parse to json

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ const gearman = require('gearman');
 const carbone = require('carbone');
 const fs = require('fs');
  
-let worker = gearman('127.0.0.1', 4730)
+let worker = gearman('192.168.1.203', 4730)
  
 // handle jobs assigned by the server
 worker.on('JOB_ASSIGN', function(job) {
     console.log(job.func_name + ' job assigned to this worker')
-    let data = job.payload.toString()
+    var messages = job.payload.toString()
 
     var options = {
         convertTo : 'pdf' //can be docx, txt, ...
     };
-     
+
+    var data = JSON.parse(messages)
+
     carbone.render('./node_modules/carbone/examples/simple.odt', data, options, function(err, result){
-        console.log("go here")
         if (err) return console.log(err);
         fs.writeFileSync('result.pdf', result);
-        process.exit(); // to kill automatically LibreOffice workers
-        
-        // notify the server the job is done
-        worker.sendWorkComplete(job.handle, result)
     });
- 
+
+        
+    // notify the server the job is done
+    worker.sendWorkComplete(job.handle, "its done")
+
     // go back to sleep, telling the server we're ready for more work
     worker.preSleep()
 });
